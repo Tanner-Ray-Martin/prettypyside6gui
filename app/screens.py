29db1e114926725df91b5591b40622621f61c29d
Tanner-Ray-Martin <tanner.ray.martin.123@gmail.com>
2023-04-30 12:12:55 -0500

    limiting-to-gifs

--- app/screens.py
+++ app/screens.py
@@ -8,10 +8,14 @@ from os import listdir
 class MainMenu(Screen):
     def __init__(self, index, name, main_window) -> None:
         super().__init__(index, name, main_window)
-        self.button_config = {
-            img_name: {"func": self.showPopup, "img": img_name}
-            for img_name in listdir(IMAGES)
-        }
+        self.button_config = dict()
+        for image_name in listdir(IMAGES):
+            if image_name.endswith(".gif"):
+                button_name = image_name.split(".")[0].title()
+                self.button_config[button_name] = {
+                    "func": self.showPopup,
+                    "img": image_name,
+                }
 
         self.initConfig()
         self.applyConfig()
