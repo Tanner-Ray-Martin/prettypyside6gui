b2eec237b5ad2bdb1648dcb2b9adffa7fc454b24
Tanner-Ray-Martin <tanner.ray.martin.123@gmail.com>
2023-04-30 17:54:46 -0500

    gif-creation-functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QStackedWidget
-from app.screens import MainMenu
+from app.screens import GifViewer, KDAExplorer, KDAWeb, KDAExplorer2
 
 
 class MainWindow(QMainWindow):
@@ -7,7 +7,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.stacked_Widget = QStackedWidget(self)
         self.monitor = app.primaryScreen
-        self.stacked_Widget.addWidget(MainMenu(0, "Main Menu", self))
+        self.stacked_Widget.addWidget(GifViewer(0, "Main Menu", self))
         self.setCentralWidget(self.stacked_Widget)
         self.stacked_Widget.show()
 
